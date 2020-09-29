deef10332d28f19c65ee5b3f2b168905500170bd
ABSphreak <absphreak@outlook.com>
2020-09-30 00:18:28 +0530

    ➕ Editor and Renderer

--- src/App.js
+++ src/App.js
@@ -1,17 +1,49 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import Markdown from 'markdown-to-jsx';
+import AceEditor from 'react-ace';
+import styled from 'styled-components';
+import 'ace-builds/src-noconflict/mode-markdown';
+import 'ace-builds/src-noconflict/theme-dracula';
 import './App.css';
 const { ipcRenderer } = window.require('electron');
 
+const Split = styled.div`
+  display: flex;
+`;
+
+const RenderedWindow = styled.div`
+  background-color: #191324;
+  width: 35%;
+  padding: 20px;
+  color: #fff;
+  border-left: 1px solid #302b3a;
+`;
+
 function App() {
+  const [loadedFile, setLoadedFile] = useState('');
+
   useEffect(() => {
     ipcRenderer.on('new-file', (event, fileContent) => {
-      console.log(fileContent);
+      setLoadedFile(fileContent);
     });
   }, []);
 
   return (
     <div className="App">
-      <h1>Hello from Electron</h1>
+      <Split>
+        <AceEditor
+          mode="markdown"
+          theme="dracula"
+          onChange={(newContent) => {
+            setLoadedFile(newContent);
+          }}
+          name="markdown_editor"
+          value={loadedFile}
+        />
+        <RenderedWindow>
+          <Markdown>{loadedFile}</Markdown>
+        </RenderedWindow>
+      </Split>
     </div>
   );
 }
